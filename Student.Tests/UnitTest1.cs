9705eec593d3dbff07354000d70bbd8e1577e247
Dell <browski999@gmail.com>
2020-05-11 16:37:49 +0100

    Added  another test

--- Student.Tests/UnitTest1.cs
+++ Student.Tests/UnitTest1.cs
@@ -14,5 +14,11 @@ namespace Student.Tests
         {
             Assert.Pass();
         }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
     }
 }
